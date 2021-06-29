a96c3cb29b2a98a2bf4ae37acd67c2bbc2dec74a
Amruta Ranade <amruta2799@gmail.com>
2021-06-29 10:49:07 -0400

    Updated index.ts for the CC narrative challenge

--- src/index.ts
+++ src/index.ts
@@ -2,57 +2,26 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import { Account } from "./entity/Account";
 
-async function insertAccount(repository, id: number, balance: number) {
-  console.log("Inserting a new account into the database...");
-
+async function insertAccount(repository, id: number, balance: number, color: string) {
+  
   let account = new Account();
   account.id = id;
   account.balance = balance;
 
   await repository.save(account);
 
-  console.log("Saved a new account.");
+  console.log(`Collected ${balance} ${color} Shime Stones.`);
 }
 
-async function printBalance(repository, id: number) {
-  console.log("Printing balances from account " + id + ".");
 
-  const account = await repository.findOne(id);
-
-  console.log(account);
-}
-
-async function transferFunds(
-  repository,
-  amount: number,
-  from: number,
-  to: number
-) {
-  console.log(`Transferring ${amount} from account ${from} to account ${to}.`);
-
-  let accountFrom = await repository.findOne(from);
-  accountFrom.balance = accountFrom.balance - amount;
-  await repository.save(accountFrom);
-
-  let accountTo = await repository.findOne(to);
-  accountTo.balance = accountTo.balance + amount;
-  await repository.save(accountTo);
-
-  console.log("Transfer complete.");
-}
 
 createConnection()
   .then(async (connection) => {
     const accountRepository = await connection.getRepository(Account);
 
-    await insertAccount(accountRepository, 1, 1000);
-    await printBalance(accountRepository, 1);
-
-    await insertAccount(accountRepository, 2, 250);
-    await printBalance(accountRepository, 2);
-
-    await transferFunds(accountRepository, 500, 1, 2);
-    await printBalance(accountRepository, 1);
-    await printBalance(accountRepository, 2);
+    await insertAccount(accountRepository, 1, 1000, 'blue');
+    await insertAccount(accountRepository, 2, 250, 'green');
+    await insertAccount(accountRepository, 3, 400, 'red');
+    
   })
   .catch((error) => console.log(error));
